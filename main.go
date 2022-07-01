5dfb43d872bce78763af8e302b36ccbc2e5653d4
taysa.barbosa <taysabarbosa2016@gmail.com>
2022-07-01 09:47:34 -0300

    Argocd

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ import "net/http"
 
 func main () {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+<<<<<<< HEAD
 		w.Write ([]byte("<h1>Hello World 1 </h1>"))
+=======
+		w.Write ([]byte("<h1>Minha aplicação com GITOPS e ArgoCD</h1>"))
+>>>>>>> 2a7e086 (ArgoCD em açãoC)
 	})
 
 	http.ListenAndServe(":8080", nil)
