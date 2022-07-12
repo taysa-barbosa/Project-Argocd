933f25d9c4aa10242dc9d1399c887d3c5d9aed9a
Taysa Barbosa <107519482+taysa-barbosa@users.noreply.github.com>
2022-07-12 11:01:40 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 func main () {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 <<<<<<< HEAD
-		w.Write ([]byte("<h1>Hello World 2 </h1>"))
+		w.Write ([]byte("<h1>Hello World 3 </h1>"))
 =======
 		w.Write ([]byte("<h1>Minha aplicação com GITOPS e ArgoCD</h1>"))
 >>>>>>> 2a7e086 (ArgoCD em açãoC)
