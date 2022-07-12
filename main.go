1ba630b80f2c09522185f13d0bb1a80672d7cb97
Taysa Barbosa <107519482+taysa-barbosa@users.noreply.github.com>
2022-07-12 11:10:50 -0300

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ import "net/http"
 
 func main () {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-<<<<<<< HEAD
+
 		w.Write ([]byte("<h1>Hello World 4 </h1>"))
-=======
-		w.Write ([]byte("<h1>Minha aplicação com GITOPS e ArgoCD</h1>"))
->>>>>>> 2a7e086 (ArgoCD em açãoC)
+
 	})
 
 	http.ListenAndServe(":8080", nil)
