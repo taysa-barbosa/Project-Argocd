26d226ad18abe40a711a628dbae40ec389d5b351
Taysa Barbosa <107519482+taysa-barbosa@users.noreply.github.com>
2022-07-12 15:30:00 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 func main () {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 
-		w.Write ([]byte("<h1>Hello World 4 </h1>"))
+		w.Write ([]byte("<h1>Hello World 5 </h1>"))
 
 	})
 
